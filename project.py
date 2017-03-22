1c4641bb72802b9bda121595665ff9c20b09d7ba
Pablo Garcia Egido <pgarciaegido@gmail.com>
2017-03-22 20:22:42 +0100

    Final project. Routes and some templates done

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Restaurant, Base, MenuItem
@@ -10,6 +10,26 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# API =========================================================================
+
+
+# Returns JSON with all menus from restaurant
+@app.route('/restaurant/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+
+# Returns JSON with only one specific menu
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def menuItemJSON(restaurant_id, menu_id):
+    item = session.query(MenuItem).filter_by(restaurant_id=restaurant_id,
+                                             id=menu_id).one()
+    return jsonify(item.serialize)
+
+
+# RENDER ======================================================================
 
 # @app.route('/')
 @app.route('/restaurant/<int:restaurant_id>')
